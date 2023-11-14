6a3eed313f3ddc15977a333a4170d5312d7b048a
SamMorgan <sam@svmorgan.com>
2023-11-15 01:17:19 +1100

    profile portrait spacing

--- template-people.php
+++ template-people.php
@@ -6,27 +6,39 @@
             <div class="people-wrap">
                 <?php 
                     if( have_rows('people') ):
-
+                    $first = true;
                     while ( have_rows('people') ) : the_row();
 
                         $portrait = get_sub_field('portrait');
                         $name = get_sub_field('name');
                         $title = get_sub_field('title');
                         $text = get_sub_field('text');?>
-
-                        <div class="person">
-                            <?php 
-                                if($portrait){
-                                    echo '<lazy-img class="portrait imgwrap"><img src="'.$portrait['url'].'" /></lazy-img>';
-                                }
-                            ?>
-                            <div class="details med-text">
-                                <h2><?php echo $name;?>, <?php echo $title;?></h2>
-                                <div class="bio"><?php echo $text;?></div>
-                            </div>    
+                        <div class="person-wrap">
+                            <div class="person">
+                                <?php 
+                                    if($portrait){
+                                        $mobile_offset = '';
+                                        if($first){
+                                            $mobile_offset = ' style="--mobile-offset:-'.(($portrait['height']/$portrait['width'])*50).'%"';
+                                        }
+                                        echo '<lazy-img class="portrait imgwrap"'.$mobile_offset.'>
+                                        <img 
+                                            src="'.$portrait['url'].'" 
+                                            width="'.$portrait['width'].'" 
+                                            height="'.$portrait['height'].'" 
+                                            style="aspect-ratio:'.$portrait['width'].'/'.$portrait['height'].'"
+                                        />
+                                        </lazy-img>';
+                                    }
+                                ?>
+                                <div class="details med-text">
+                                    <h2><?php echo $name;?>, <?php echo $title;?></h2>
+                                    <div class="bio"><?php echo $text;?></div>
+                                </div>    
+                            </div>
                         </div>
-
-                    <?php endwhile;
+                    <?php $first = false; 
+                    endwhile;
 
                     endif;
                 ?>
