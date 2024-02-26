72a90cdf45d85245de4af4f4a4e5774302342141
SamMorgan <sam@svmorgan.com>
2024-02-27 10:41:59 +1100

    fixed favicon path

--- header.php
+++ header.php
@@ -11,11 +11,11 @@
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <meta name="format-detection" content="telephone=no"> 
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/images/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/images/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/images/favicon/favicon-16x16.png">
-    <!-- <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/images/favicon/site.webmanifest"> -->
-    <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon/safari-pinned-tab.svg" color="#000000">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon/favicon-16x16.png">
+    <!-- <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/favicon/site.webmanifest"> -->
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/favicon/safari-pinned-tab.svg" color="#000000">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="theme-color" content="#ffffff">
     <script>
