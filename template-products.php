d4d9e3155204db5f7e1d3e8fffec52d5c391b85a
Sam Morgan <sam@svmorgan.com>
2024-05-20 17:23:20 +1000

    products index css

--- template-products.php
+++ template-products.php
@@ -3,10 +3,16 @@
     get_header(); ?>
     <?php $products = get_field('products');
     if( $products ): ?>
-    <div class="work-index archive">
-    <?php foreach( $products as $post ): 
-        get_template_part('includes/project-card');           
-    endforeach; ?>
+    <div class="product-index">
+    <?php foreach( $products as $post ): ?>
+        <div class="product-card allcaps">
+        <a href="<?php the_permalink();?>" class="tooltip">
+            <div class="imgwrap"><?php the_post_thumbnail('full');?></div>
+            <h3><?php the_title();?></h3>
+            <span><?php the_field('year');?></span>
+        </a>
+        </div>          
+    <?php endforeach; ?>
     </div>
     <?php wp_reset_postdata();
     endif;?>
